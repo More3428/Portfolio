01f8599f162b26b3e806da37bbf57578d2435e3a
Andre Moreno <80718143+More3428@users.noreply.github.com>
2024-08-12 19:27:27 -0700

    Project Page Done Working on Contact Page
    Project page is basically finished.
    Contact page needs work but functionality is complete

--- my-portfolio/src/app/components/Projects/page.tsx
+++ my-portfolio/src/app/components/Projects/page.tsx
@@ -77,6 +77,9 @@ const Projects = () => {
                                 </div>
                                 <div className="font-mono text-left">
                                     <h2 className="font-custom2 text-3xl underline decoration-1">MBARI Modernization Project</h2>
+                                    
+                                    <p className="text-sm font-semibold">Full Stack Application</p>
+                                    <p className="text-sm">Worked on Front-End Development and Back-End integration.</p>
                                     <p className="text-sm">Created a program with a team of 4 where MBARI is able to log their expeditions and gather data.</p>
                                     <p className="text-sm">Worked on Front-End Development and Back-End integration.</p>
                                     <p className="text-sm">Created a UI which allowed for an easy user-experience</p>
@@ -96,10 +99,30 @@ const Projects = () => {
                                 </div>
                                 <div className="font-mono text-left">
                                     <h2 className="font-custom2 text-3xl underline decoration-1">Portfolio</h2>
+                                    <p className="text-sm font-semibold">Web Development</p>
                                     <p className="text-sm">Created this portfolio to showcase my work as a software engineer.</p>
                                     <p className="text-sm">Here you will learn more about me and what I've been up to since graduating college in december of 2023</p>
-                                    <p className="text-sm">This is just one of many projects.</p>
+                                    <p className="text-sm">Mostly used front-End development on this portfolio project.</p>
                                     <h3>Tools: React, Next.js, TypeScript, TailwindCSS, NextUI  </h3>
+                                </div>
+                            </div>
+
+                            <div className="grid grid-cols-[auto,1fr] gap-4 bg-slate-900 p-2 mb-4 rounded-lg">
+                                <div className="p-2"> 
+                                    <img src="/assets/journal-man-11.png" alt="bash" className='w-30 h-auto max-w-full max-h-40 rounded drop-shadow-md' />
+                                    <div className="flex space-x-4 pb-2">
+                                        <p className="underline font-mono">Learn More</p>
+                                        <a href="https://github.com/MBARI-capstone/front-end" target="_blank" rel="noopener noreferrer">
+                                        <p className="underline font-mono">Github</p>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="font-mono text-left">
+                                    <h2 className="font-custom2 text-3xl underline decoration-1">Take a Walk</h2>
+                                    <p className="text-sm font-semibold">Full-stack application</p>
+                                    <p className="text-sm">Front/Back End Development </p>
+                                    <p className="text-sm">Social media app that allows users to post and map their journeys. Used React-native maps and geolocation features for user mapping.</p>
+                                    <h3>Tools: React Native, Node.js, MongoDb</h3>
                                 </div>
                             </div>
 
@@ -115,29 +138,11 @@ const Projects = () => {
                                 </div>
                                 <div className="font-mono text-left">
                                     <h2 className="font-custom2 text-3xl underline decoration-1">Redis Clone in C/C++</h2>
+                                    <p className="text-sm font-semibold">DSA and Network Programming</p>
                                     <p className="text-sm">Created a clone of Redis from scratch using C/C++.</p>
                                     <p className="text-sm">Created an in Memory data structure store.</p>
                                     <p className="text-sm">Data Structures and Network Programming</p>
                                     <h3>Tools: C/C++  </h3>
-                                </div>
-                            </div>
-
-                            <div className="grid grid-cols-[auto,1fr] gap-4 bg-slate-900 p-2 mb-4 rounded-lg">
-                                <div className="p-2"> 
-                                    <img src="/assets/mbarilogo.png" alt="bash" className='w-40 h-auto max-w-full max-h-40 bg-white rounded drop-shadow-md' />
-                                    <div className="flex space-x-4 pb-2">
-                                        <p className="underline font-mono">Learn More</p>
-                                        <a href="https://github.com/MBARI-capstone/front-end" target="_blank" rel="noopener noreferrer">
-                                        <p className="underline font-mono">Github</p>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="font-mono text-left">
-                                    <h2 className="font-custom2 text-3xl underline decoration-1">MBARI Modernization Project</h2>
-                                    <p className="text-sm">Created a program with a team of 4 where MBARI is able to log their expeditions and gather data.</p>
-                                    <p className="text-sm">Worked on Front-End Development and Back-End integration.</p>
-                                    <p className="text-sm">Created a UI which allowed for an easy user-experience</p>
-                                    <h3>Tools: React, Next.js, TypeScript, Java, Springboot, SQL  </h3>
                                 </div>
                             </div>
 
